83ed43cde7e1e3776aa9c101d495c532366914ea
root <root@localhost.localdomain>
2018-06-25 15:54:06 -0400

    Still need to add code for handling the second dataset (CPU Usage %). The changes made attempt to handle the first dataset.

--- grabData.py
+++ grabData.py
@@ -23,7 +23,7 @@ parser = argparse.ArgumentParser()
     line = fp.readline()
     x_vals = [int(s) for s in line.split() if s.isdigit()]
 
-    #This is the list of lists
+    # This is the list of lists
     all_trace = [[] for l in xrange(total_datasets)]
 
     for x in xrange(total_datasets):
@@ -32,37 +32,74 @@ parser = argparse.ArgumentParser()
         chart_type = line_split(1)
         if chart_type == 'line':
             for y in xrange(poi):
+		# Appending 'down'
                 all_trace[x].append(go.Scatter(x = x_vals))
         elif chart_type == 'bar':
-            for y in  xrange(poi):
+            for y in xrange(poi):
+		# Appending 'down'
                 all_trace[x].append(go.Bar(x = x_vals))
         else:
             exit(1)
 
         for y in xrange(poi):
             line = fp.readline()
-	    line_split2 = line.split()
+	    line_split = line.split()
 	    y_title = line.replace('# y-title', '')
-	    # Should the x_title above be line.replace('\n', '')?
-	    y_title = line.replace('\n', '')
+	    y_title = y_title.replace('\n', '')
 	    line = fp.readline()
-	    through_vals = [int(s) for s in line.split() if s.isdigit()]
-	    # Now take those values (above)  and plot them.
+	    throughput_vals = [int(s) for s in line.split() if s.isdigit()]
+	    # Now take throughput_vals and plot them
 	    if chart_type == 'line':
 		for y in xrange(poi):
-		    all_trace[y].append(go.Scatter(y = through_vals)
+		    all_trace[x].append(go.Scatter(y = throughput_vals)
 	    elif chart_type == 'bar':
 		for y in xrange(poi):
-		    all_trace[y].append(go.Bar(y = through_vals)
+		    all_trace[x].append(go.Bar(y = throughput_vals)
 	    fp.readline()
 	    line = fp.readline()
-	    dev_vals = [int(s) for s in line.split() if s.isdigit()]
-	    #Then plot based on chart_type?
-	    #Do the same thing for STDERR?
-	    fp.readline()
-	    line = fp.readline()
-	    err_vals = [int(s) for s in line.split() if s.isdigit()]
-	    #Then plot based on chart_type?
-            # Grab everything out of the file and plot it all. Put all 3 points of interest in the same graph and plot it. 
-            
-
+	    # Get standard deviation values
+	    stdev_vals = [int(s) for s in line.split() if s.isdigit()]
+	    if chart_type == 'bar':
+		# Ignore standard deviation
+		fp.readline()
+		line = fp.readline()
+		# Get standard error values
+		err_vals = [int(s) for s in line.split() if s.isdigit()]
+		# Plot error bars
+		all_trace[x].error_y = dict(
+		    type = 'data',
+		    array = [err_vals],
+		    visible = True
+		)
+	    elif chart_type == 'line':
+		# Bounds for standard deviation
+		upper_bound = go.Scatter(
+		    name = 'Upper Bound',
+		    x = x_vals,
+		    y = throughput_vals + stdev_vals,
+		    mode = 'lines',
+		    marker = dict(color = '#444'),
+		    line = dict(width = 0),
+		    fillcolor = 'rgba(68, 68, 68, 0.3)',
+		    fill = 'tonexty'
+		)
+		lower_bound = go.Scatter(
+		    name = 'Lower Bound',
+		    x = x_vals,
+		    y = throughput_vals - stdev_vals,
+		    mode = 'lines',
+		    marker = dict(color = '#444'),
+		    line = dict(width = 0)
+		# Possibly add fillcolor and fill
+		)
+	        fp.readline()
+	        line = fp.readline()
+		# Is it necessary to get standard error values again?
+	        err_vals = [int(s) for s in line.split() if s.isdigit()]
+		# Plot error bars
+		all_trace[x].error_y = dict(
+                    type = 'data',
+                    array = [err_vals],
+                    visible = True
+                )
+	    # Now we have to put together a 2nd graph for CPU Percentage?
